e0f19dea2234fcf4cf42ca9800b931fdf74a18e3
neeerajtk <neerajtk560@gmail.com>
2020-03-29 05:57:09 +0000

    first express code added...yeyyyy

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -1,1 +1,12 @@
-console.log("hello");
+var express = require("express");
+var app = express();
+
+app.get("/",function(req, res){
+	res.send("Hi there");
+});
+app.get("/bye",function(req, res){
+	res.send("bye");
+});
+app.listen(3000,function(){
+	console.log("Serving demo ");
+})
