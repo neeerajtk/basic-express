aa9f643e7f22dd76c9c9143836d1b6ed38fd3bad
neeerajtk <neerajtk560@gmail.com>
2020-03-29 18:23:38 +0000

    custom assets added

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine","ejs");
 app.get("/", function(req,res){
-	res.render("home.ejs");
+	res.render("home");
 });
 app.get("/fallinlovewith/:thing",function(req,res){
 	var thing = req.params.thing;
-	res.render("love.ejs",{thingVar: thing});
+	res.render("love",{thingVar: thing});
 	
 });
 app.get("/posts",function(req, res){
@@ -15,7 +17,7 @@ app.get("/posts",function(req, res){
 		{title: "Post 2", author: "Neeraj"},
 		{title: "Post 3", author: "Arun"},
 	];
-	res.render("posts.ejs",{posts: posts});
+	res.render("posts",{posts: posts});
 });
 
 app.listen(3000,function(){
