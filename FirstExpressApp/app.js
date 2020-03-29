96055b14d194a25dec0beb637d0f64ab356ea010
neeerajtk <neerajtk560@gmail.com>
2020-03-29 06:10:52 +0000

    added more functionalities tp first

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -2,9 +2,11 @@ var express = require("express");
 var app = express();
 
 app.get("/",function(req, res){
+	console.log("Someone made a request to /");
 	res.send("Hi there");
 });
 app.get("/bye",function(req, res){
+	console.log("Someone made a request to /bye");
 	res.send("bye");
 });
 app.listen(3000,function(){
