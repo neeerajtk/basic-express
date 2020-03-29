10a01051af7023037907c3ed23198cd114bd14db
neeerajtk <neerajtk560@gmail.com>
2020-03-29 07:10:23 +0000

    route param added

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -9,6 +9,19 @@ app.get("/bye",function(req, res){
 	console.log("Someone made a request to /bye");
 	res.send("bye");
 });
+app.get("/r/:subredditName",function(req,res){
+	console.log(req.params);
+	var subreddit = req.params.subredditName;
+	
+	res.send("Welcome to the "+ subreddit.toUpperCase() +" page.");
+});
+app.get("/r/:subredditName/comments/:id/:title/", function(req,res){
+	res.send("welcome to comments");
+});
+
+app.get("*",function(req, res){
+	res.send("You are a star");
+});
 app.listen(3000,function(){
 	console.log("Serving demo ");
 })
