bc95f12194d6bd3cf4e2b6d32d9d551351180158
neeerajtk <neerajtk560@gmail.com>
2020-04-03 12:19:50 +0530

    association mongoose error

--- associations/embed.js
+++ associations/embed.js
@@ -1,1 +1,19 @@
-var
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/blog_demo");
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String
+});
+
+var User = mongoose.model("User",userSchema);
+
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+var postModel = mongoose.model("Post",postSchema);
